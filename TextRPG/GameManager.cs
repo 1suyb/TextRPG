21501b060a7bae98891cdb3d34fee3b3c714cfff
1suyb <suyb1234@gmail.com>
2024-09-21 20:53:28 +0900

    <Feat> UserInput 기능 추가 및 테스트 케이스 제작
    UserInput및 게임전체적인 Scene관리를 위한 GameManager와 이를 테스트하기 위한 간단한 테스트 케이스 작성

---
+++ TextRPG/GameManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+	public enum GameState
+	{
+		Start = -1,
+		Main = 0,
+		Status = 1,
+		Inventory = 2,
+		Shop = 3,
+	}
+	internal class GameManager
+	{
+		private GameState _state;
+
+		public GameManager()
+		{
+			_state = GameState.Start;
+		}
+
+		public void GameMain()
+		{
+			while (true)
+			{
+				switch (_state)
+				{
+					case GameState.Start:
+						StartScene();
+						break;
+					case GameState.Main:
+						MainScene();
+						break;
+					case GameState.Status:
+						StatusScene();
+						break;
+					case GameState.Inventory:
+						InventoryScene();	
+						break;
+					case GameState.Shop:
+						ShopScene();
+						break;
+
+				}
+			}
+		}
+		public int Input()
+		{
+			string? input = "";
+			while (input == null || input == "")
+			{
+				input = Console.ReadLine();
+			}
+			return int.Parse(input);
+		}
+		
+		public void StartScene()
+		{
+			Console.WriteLine("어서오세요 TextRPG입니다");
+			_state = GameState.Main;
+		}
+		public void MainScene()
+		{
+			Console.WriteLine("1. 상태보기");
+			Console.WriteLine("2. 인벤토리");
+			Console.WriteLine("3. 상점");
+			Console.WriteLine();
+			Console.WriteLine("원하시는 행동을 입력해주세요");
+			Console.Write(">>>");
+			GetUserInputInMainScene();
+		}
+		public void GetUserInputInMainScene()
+		{
+			int userInput = 0;
+			while (true)
+			{
+				userInput = Input();
+				Console.WriteLine(userInput);
+				Console.WriteLine((GameState)userInput);
+				if (userInput <= 0 || userInput > 3)
+				{
+					Console.WriteLine(" 잘못된 입력입니다. ");
+				}
+				else
+				{
+					break;
+				}
+				
+			}
+			_state = (GameState)userInput;
+		}
+		public void StatusScene()
+		{
+			Console.WriteLine("Debug - Staus 진입");
+			_state = GameState.Main;
+		}
+		public void InventoryScene()
+		{
+			Console.WriteLine("Debug - Inventory 진입");
+			_state = GameState.Main;
+		}
+		public void ShopScene()
+		{
+			Console.WriteLine("Debug - Shop 진입");
+			_state = GameState.Main;
+		}
+
+	}
+}
